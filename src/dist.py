c521f8be72195193e818df7729ded85d79874235
junweisow789 <junweisow789@gmail.com>
2017-02-13 12:44:47 +0000

    modified:   src/dist.py

--- src/dist.py
+++ src/dist.py
@@ -1,42 +1,45 @@
 #----------------------------HC-SR04 Ultrasonic Rangefinder--------------------------------
-import machine, time
-from machine import Pin
+def start():
+	import machine, time
+	from machine import Pin
 
-# ESP8266 pins 0, 2, 4, 5, 12, 13, 14 and 15 are all GPIO
-#initialise pins for trigger output and echo input
-trigger_pin = 0
-echo_pin = 2
 
-#initialise pins
-echo_timeout_us = 20 #Max time to return echo in us
-trigger = Pin(trigger_pin, mode = Pin.OUT, pull=none)
-echo = Pin(echo_pin, mode=Pin.IN, pull=None)
+	# ESP8266 pins 0, 2, 4, 5, 12, 13, 14 and 15 are all GPIO
+	#initialise pins for trigger output and echo input
+	trigger_pin = 0
+	echo_pin = 2
 
-trigger.low() #documentation does not have low. Try value() instead
+	#initialise pins
+	echo_timeout_us = 20 #Max time to return echo in us
+	trigger = Pin(trigger_pin, mode = Pin.OUT, pull=None)
+	echo = Pin(echo_pin, mode=Pin.IN, pull=None)
 
-def send_pulse_measure_dist():
+	trigger.low() #documentation does not have low. Try value() instead
+	print ("pin initialisation completed")
+	def send_pulse_measure_dist():
 
-	# Send Trigger out
-	trigger.low() # Stabilize the sensor
-    time.sleep_us(5)
-    trigger.high()
-    # Send a 10us pulse.
-    time.sleep_us(10)
-    trigger.low()
-    try:
-        pulse_time = machine.time_pulse_us(echo_pin, 1, echo_timeout_us)
-        mm = pulse_time * 100 // 582
-        print mm
-    except OSError as ex:
-        if ex.args[0] == 110: # 110 = ETIMEDOUT
-            raise OSError('Out of range')
-        raise ex
-    #Receive pulse echo back and calculate distance
+		# Send Trigger out
+		trigger.low() # Stabilize the sensor
+		time.sleep_us(5)
+		print ("sending pulse...")
+		trigger.high()
+	    # Send a 10us pulse.
+		time.sleep_us(10)
+		trigger.low()
+		try:
+	    	pulse_time = machine.time_pulse_us(echo, 1, 20)
+	    	mm = pulse_time * 100 // 582
+	    	print ("distance from the target:", mm, "mm")
+		except OSError as ex:
+	    	if ex.args[0] == 110: # 110 = ETIMEDOUT
+	        	raise OSError('Out of range')
+	    	raise ex
+	    #Receive pulse echo back and calculate distance
 
-    # To calculate the distance we get the pulse_time and divide it by 2
-    # (the pulse walk the distance twice) and by 29.1 becasue
-    # the sound speed on air (343.2 m/s), that It's equivalent to
-    # 0.34320 mm/us that is 1mm each 2.91us
-    # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582
+	    # To calculate the distance we get the pulse_time and divide it by 2
+	    # (the pulse walk the distance twice) and by 29.1 becasue
+	    # the sound speed on air (343.2 m/s), that It's equivalent to
+	    # 0.34320 mm/us that is 1mm each 2.91us
+	    # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582
 
-send_pulse_measure_dist()
+	send_pulse_measure_dist()
